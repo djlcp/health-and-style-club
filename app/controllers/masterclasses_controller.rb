1f34679121715b7abdd56765baa36e52395afc51
Kristin Morris <kristinmorris@hotmail.co.uk>
2018-04-15 22:00:23 +0100

    Created Masterclass Controller and added View Files for Masterclass

---
+++ app/controllers/masterclasses_controller.rb
@@ -0,0 +1,75 @@
+class MasterclassesController < ApplicationController
+
+  def index
+    @masterclasses = masterclass.all
+  end
+
+  def show
+    @masterclass = masterclass.find(params[:id])
+  end
+
+  def new
+    @masterclass = masterclass.new
+  end
+
+  def create
+    @masterclass = masterclass.new(params.require(:post).permit(:title, :body_text))
+
+    respond_to do |format|
+        if @masterclass.save
+          format.html { redirect_to masterclass_path(@masterclass), notice: 'Masterclass added.'}
+        else
+          format.html { render :new}
+          end
+        end
+      end
+
+      def edit
+        @masterclass = masterclass.find(params[:id])
+
+      end
+
+      def update
+        @masterclass = masterclass.find(params[:id])
+        respond_to do |format|
+          if @masterclass.update(params.require(:post).permit(:title, :body_text))
+            format.html { redirect_to @masterclass, notice: 'Masterclass updated.' }
+          else
+            format.html { render :edit }
+
+          end
+        end
+      end
+
+
+
+      def destroy
+        @masterclass = Masterclass.find(params[:id])
+        @masterclass.destroy
+        respond_to do |format|
+          format.html { redirect_to masterclass_path, notice: 'The Masterclass was deleted.' }
+          format.json { head :no_content }
+
+      end
+      end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :body_text)
+    end
+
+    def set_link
+        @masterclass = Masterclass.find(params[:id])
+    end
+
+
+
+    end
+
+
+
+
+
+
+end
