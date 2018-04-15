8413b2d4ee213b16f4abd78708b1ae5d0ba080a2
Liam Borner <liamborner@gmail.com>
2018-04-15 16:59:24 +0100

    Added posting function to allow full crud operations. Not yet locked down with authentication, but the basics are in so we can now add, edit, and delete posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
   get 'home/index'
 
   devise_for :users
   devise_for :installs
   devise_for :models
+  resources :posts
+  
 
   root to: 'home#index'
+
 
 end
