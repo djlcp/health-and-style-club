f5f34b5cc8065d49deaddba8ad5e01a47334a905
Raymond Cochrane <raymondcochrane@outlook.com>
2018-04-19 13:49:07 +0100

    add users controller and html

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users
+  scope '/admin' do
+    resources :users
+  end
   resources :posts
   resources :invoices
 
 
 
-  root to: 'home#index'
+  root to: 'users#index'
 
 
 end
