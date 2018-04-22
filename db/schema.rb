ad891fc692d7d31e1e40f35975caf749ddbf6e15
Liam Borner <liamborner@gmail.com>
2018-04-22 17:39:45 +0100

    added in a content table and content model to interact with posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422131700) do
+ActiveRecord::Schema.define(version: 20180422163124) do
+
+  create_table "content", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "content_type"
+    t.string "description"
+    t.string "image_url"
+    t.string "video_url"
+    t.string "doc_url"
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user"
@@ -23,12 +34,13 @@ ActiveRecord::Schema.define(version: 20180422131700) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
-    t.string "body_text"
+    t.text "body_text", limit: 4294967295
     t.boolean "paid_for"
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.string "user_id"
+    t.integer "user2_id"
   end
 
   create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
