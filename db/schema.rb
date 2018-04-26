3865b36debdc84ca99cce831006962830531dea4
Raymond Cochrane <raymondcochrane@outlook.com>
2018-04-26 19:41:13 +0100

    added carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426180252) do
+ActiveRecord::Schema.define(version: 20180426183205) do
 
   create_table "content", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "content_type"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180426180252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
