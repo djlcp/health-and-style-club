36404b5a6784d3c460c9df34808a75461ad5b611
jjjjoooonnnn <joncaws@gmail.com>
2018-04-30 14:45:39 +0100

    Changed descriptions in posts and contents to text. Removed redundant order column.

---
+++ db/migrate/20180430133334_change_content_description_and_post_text.rb
@@ -0,0 +1,7 @@
+class ChangeContentDescriptionAndPostText < ActiveRecord::Migration[5.1]
+  def change
+    change_column :contents, :description, :text
+    remove_column :contents, :order
+    change_column :posts, :body_text, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427173652) do
+ActiveRecord::Schema.define(version: 20180430133334) do
 
   create_table "contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "content_type"
-    t.string "description"
-    t.integer "order"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "file_url"
@@ -40,17 +39,13 @@ ActiveRecord::Schema.define(version: 20180427173652) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
-    t.string "body_text"
+    t.text "body_text"
     t.boolean "paid_for"
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "user2_id"
     t.string "image"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -74,8 +69,9 @@ ActiveRecord::Schema.define(version: 20180427173652) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "subscription_id"
     t.string "first_name"
     t.string "surname"
     t.string "middle_names"
