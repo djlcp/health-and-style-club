4f1ad3f667f614957ce1d654a769ff8019590822
Tiago Soares <tiagosoaresjersey@gmail.com>
2018-05-03 16:57:21 +0100

    Attempting to implement webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :subscriptions
 
-  scope '/hooks', :controller => :hooks do
+  scope '/hooks', :controller => :subscriptions do
     post :subscription_created_callback
   end
 
