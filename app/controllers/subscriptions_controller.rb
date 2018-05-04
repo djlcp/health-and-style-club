e2563c8065f67ef7380258bfa15c9083cf2221de
Tiago Soares <tiagosoaresjersey@gmail.com>
2018-05-04 09:03:01 +0100

    Webhooks addition

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,8 +6,8 @@ def subscription_created_callback
   if params[:status].present?
     # Create a new Survey object based on the received parameters...
     subscription = Subscription.new(:status => params[:paid_for]
-    subscription.url = params[:subscription_url]
-    subscription.creator_email = params[:subscription_creator_email]
+    # subscription.url = params[:subscription_url]
+    # subscription.creator_email = params[:subscription_creator_email]
     subscription.save!
   end
   #
