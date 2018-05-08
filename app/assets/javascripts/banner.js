ec1fe7a527c1a07c0857458342bbb8026fa04837
Carys Williams <cwilliams52093@gmail.com>
2018-05-08 18:55:32 +0100

    Expertbar image fixed to show automatically on page load

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -1,28 +1,34 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener("turbolinks:load", function() {
+  var slideIndex = 1;
+  showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1
+    } 
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex -1].style.display = "block"; 
+    dots[slideIndex -1].className += " active";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+
+  $('.js-dot').on('click', function() {
+    var number = $(this).data('number')
+    showSlides(slideIndex = number)
+  });
+
+  $('.js-next, .js-prev').on('click', function() {
+    var number = $(this).data('number')
+    showSlides(slideIndex += number)
+  })
+});
