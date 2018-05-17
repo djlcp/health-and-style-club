bdf8221cfa96c37bac69b62eaa7f5f43db965d97
Carys Williams <cwilliams52093@gmail.com>
2018-05-17 18:28:00 +0100

    Deleted scaffold and fixed sign up error

--- db/schema.rb
+++ db/schema.rb
@@ -107,7 +107,6 @@ ActiveRecord::Schema.define(version: 20180512142350) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "role"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -116,8 +115,9 @@ ActiveRecord::Schema.define(version: 20180512142350) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "subscription_id"
     t.string "first_name"
     t.string "surname"
     t.string "middle_names"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20180512142350) do
     t.string "state"
     t.string "country"
     t.string "post_code"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
