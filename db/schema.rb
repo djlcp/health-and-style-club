9beb39eb5ea1e9c94e87893c20c489da380162f5
jjjjoooonnnn <joncaws@gmail.com>
2018-05-22 18:34:29 +0100

    Migration updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 20180726203205) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "post_id"
-    t.integer "user_id"
-    t.text "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -114,7 +112,6 @@ ActiveRecord::Schema.define(version: 20180726203205) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "role"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -123,8 +120,9 @@ ActiveRecord::Schema.define(version: 20180726203205) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "subscription_id"
     t.string "first_name"
     t.string "surname"
     t.string "middle_names"
@@ -135,8 +133,7 @@ ActiveRecord::Schema.define(version: 20180726203205) do
     t.string "state"
     t.string "country"
     t.string "post_code"
-    t.string "bio"
-    t.string "bioavatar"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
