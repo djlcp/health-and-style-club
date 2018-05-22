a2cf7bb831af62a6571c35c077aa929f137c98d6
Tiago Soares <tiagosoaresjersey@gmail.com>
2018-05-22 20:43:43 +0100

    Added Unique user_id in Subscriptions model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :variables
   # protect_from_forgery with: :exception
 
+
+  #These variables are available in the whole app
+  #
+  # @signed_in      User signed in?
+  # @paid           Subscription paid for?
+  # @subscription   User's subscription
+  # @sub            Subscription exists?
+  #
   def variables
 
     if user_signed_in?
