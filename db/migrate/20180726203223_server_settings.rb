9b2ae1cf374c66e68a1c2603b7db542176b66b58
Raymond Cochrane <raymondcochrane@outlook.com>
2018-06-27 21:28:06 +0100

    merge fix

--- db/migrate/20180726203223_server_settings.rb
+++
@@ -1,8 +0,0 @@
-class ServerSettings < ActiveRecord::Migration[5.1]
-    def change
-      create_table :serversettings do |t|
-        t.boolean :active, index: true
-        t.integer :reactivation_date
-    end
-  end
-end
