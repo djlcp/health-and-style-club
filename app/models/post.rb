74eba5c76c456783cbd3ddd945335f927efe25d9
jjjjoooonnnn <joncaws@gmail.com>
2018-06-28 19:03:49 +0100

    uncommented some things

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,11 @@ class Post < ApplicationRecord
   acts_as_votable
   mount_uploader :image, Picture
 
-  # scope :category, -> (category) { where category_id: category }
+  scope :category, -> (category) { where category_id: category }
 
   belongs_to :user
-  # belongs_to :category
+  belongs_to :category
   # belongs_to :masterclass
   has_many :comments
-
-
 
 end
