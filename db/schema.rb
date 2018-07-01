0fdc89f1ff7ed6ec04d305b1e0ec2b5fc5a3111c
Bill Rogers <billyrogers902003@yahoo.co.uk>
2018-07-01 22:39:16 +0100

    Styling changes for the members masterclass index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726203240) do
+ActiveRecord::Schema.define(version: 20180726203239) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -82,14 +82,6 @@ ActiveRecord::Schema.define(version: 20180726203240) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "post_id"
-    t.integer "content_id"
-    t.integer "order"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "body_text"
@@ -97,20 +89,12 @@ ActiveRecord::Schema.define(version: 20180726203240) do
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.string "image"
     t.integer "category_id"
     t.string "post_description"
     t.integer "likes"
-    t.integer "user_id"
     t.integer "category"
-    t.integer "sub_category"
-  end
-
-  create_table "serversettings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.boolean "active"
-    t.date "reactivation_date"
-    t.string "task_name"
-    t.index ["active"], name: "index_serversettings_on_active"
   end
 
   create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -118,8 +102,8 @@ ActiveRecord::Schema.define(version: 20180726203240) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "paid_for"
     t.string "chargebee_id"
-    t.boolean "paid_for", default: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -172,6 +156,7 @@ ActiveRecord::Schema.define(version: 20180726203240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "videoable_id"
+    t.boolean "welcome_video", default: false
     t.string "videoable_type"
   end
 
