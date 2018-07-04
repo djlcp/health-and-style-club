80e23bf727a4ea71ad94d6e81ff3b66ddadb9bac
Bill Rogers <billyrogers902003@yahoo.co.uk>
2018-07-04 09:30:30 +0100

    Add in missing table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726203239) do
+ActiveRecord::Schema.define(version: 20180726203241) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -93,7 +93,14 @@ ActiveRecord::Schema.define(version: 20180726203239) do
     t.string "image"
     t.integer "category_id"
     t.string "post_description"
+    t.integer "likes"
     t.integer "category"
+    t.integer "sub_category"
+  end
+
+  create_table "serversettings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
