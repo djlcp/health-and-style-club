6366c25c2fd52213a61fcdf1a0ab990656e79ff2
Tiago Soares <tsoares@localhost.localdomain>
2018-07-04 20:05:58 +0100

    Changed speling mistake

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,14 @@
 # class RegistrationsController < Devise::RegistrationsController
 # 	before_filter :configure_account_update_params, only: [:update]
-#
+
 # 	protected
-#
+
 # 	def configure_account_update_params
 # 		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :current_password) }
 # 	end
-#
+
 # 	def update_resource(resource, params)
 # 		resource.update_without_password(params)
 # 	end
-#
+
 # end
